b98773bfde07dc7a6fb24dd25b80e4817725294b
Gregory Williams <gregoryew@gmail.com>
2016-04-10 09:07:23 -0400

    cleaned up data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,14 +21,11 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
-	var match;
-	todos.forEach(function (todo) {
-		if (todo.id === todoId) {
-			match = todo;
-		}
-	});
-	if (match) {
-		res.json(match);
+
+	var matchedToDo = _.findWhere(todos, {id: todoId});
+
+	if (matchedToDo) {
+		res.json(matchedToDo);
 	} else {
 		res.status(404).send();
 	}
@@ -35,11 +33,16 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
-	//add id field
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	body.id = todoNextId++;
-	//push body into array
+
 	todos.push(body);
 
 	res.json(body);
